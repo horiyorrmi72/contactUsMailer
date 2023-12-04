aaa21801fa6a13be8c2c6e203ec63bc303ed6780
yusuf <horiyorrmi72@gmail.com>
2023-12-04 18:45:57 +0100

    mailer

---
+++ app.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const corsOptions = {
+  origin: process.env.SITE_ORIGIN_URL,
+  optionsSuccessStatus: 200,
+};
+
+app.post("/submit-form", cors(corsOptions), (req, res) => {
+  const { name, subject, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.USER_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
+  });
+  // console.log(process.env.USER_EMAIL);
+
+  const mailOptions = {
+    from: `my-portfolio - ${email}`,
+    to: process.env.USER_EMAIL,
+    subject: subject,
+    text: `from: ${email} - \n Message: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      // console.log(err);
+      res.status(500).send({ message: "Error sending email" });
+    } else {
+      // console.log("Sent mail: " + info.response);
+      res.status(200).send({ message: "Email sent successfully" });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
